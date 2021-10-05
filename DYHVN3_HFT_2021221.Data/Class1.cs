f47358462903ea0671e0e294e0b3988d69ddecb8
barnai <barnai@stud.uni-obuda.hu>
2021-10-05 18:04:19 +0200

    -Database connection string placed to Data Class as comment. -Microsoft.EntityFrameworkCore Nuggets installed for Lazy Loading etc.

--- DYHVN3_HFT_2021221.Data/Class1.cs
+++ DYHVN3_HFT_2021221.Data/Class1.cs
@@ -4,5 +4,6 @@ namespace DYHVN3_HFT_2021221.Data
 {
     public class Class1
     {
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True
     }
 }
