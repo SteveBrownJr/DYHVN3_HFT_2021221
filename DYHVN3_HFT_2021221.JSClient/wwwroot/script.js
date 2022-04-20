26f51a1195821728308152aa1b39b66f4e96f154
barnai <barnai@stud.uni-obuda.hu>
2022-04-20 22:13:10 +0200

    Get is completed.

--- DYHVN3_HFT_2021221.JSClient/wwwroot/script.js
+++ DYHVN3_HFT_2021221.JSClient/wwwroot/script.js
@@ -1,3 +1,22 @@
-﻿fetch('http://localhost:38193/Locomotive')
+﻿let locomotives = [];
+
+fetch('http://localhost:38193/Locomotive')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        locomotives = y;
+        console.log(locomotives);
+        display();
+    });
+
+function display() {
+    locomotives.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.locomotive_Id + "</td><td>"
+        + t.name + "</td><td> "
+        + t.type + "</td><td> "
+        + t.staff + " fő</td><td> "
+        + t.starting_Torque + " kN </td><td> "
+        + t.load + " tonna"
+        + "</td></tr>"
+    });
+}
